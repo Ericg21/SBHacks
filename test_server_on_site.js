14717f8c9aa8316b53823218e208429dcd6daeda
AndrewJGaut <ajg@umail.ucsb.edu>
2019-01-12 12:57:02 -0800

    my code

---
+++ test_server_on_site.js
@@ -0,0 +1,129 @@
+var MyMongoose = require('./generalized_exportable_mongoose.js');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+//Set up mongoose connection
+var mongoDB = "mongodb://localhost:27017/test";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise; //make sure using correct library
+
+//get the database instance
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+
+
+
+/*
+SCHEMAS
+*/
+Schema = mongoose.Schema;
+
+var ProfileSchema = new Schema
+({
+	username: String,
+	firstname: String,
+	lastname: String,
+	password: String,
+	projects: [ { _id: Schema.Types.ObjectId, name: String} ] 
+});
+
+//define a virtul property for the schema (don't have to concatenate name every time
+ProfileSchema.virtual('fullname').get(function() {
+	return this.firstname + " " + this.lastname;
+});
+
+//define a function for all profile objects
+ProfileSchema.statics.findByFullName = function(name) {
+	return this.find({ fullname: new RegExp(name, 'i') });
+};
+
+//define a function for all profile objects
+ProfileSchema.statics.findById = function(id) {
+	return this.findOne( { _id: id} ); 
+};
+
+
+
+/*
+MODELS (these are collections in the mongoDB database)
+*/
+//PROFILES
+var Profile = mongoose.model('Profile', ProfileSchema);
+
+
+/*
+WEBSITE COMMUNICATION
+*/
+var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/',function(req,res)
+{
+	res.send("MAIN PAGE");
+});
+
+app.get('/signin',function(req,res)
+{
+	res.send("SIGNIN");
+});
+
+app.get('/databasetests',function(req,res)
+{
+	var brent_query = Profile.findOne( {firstname: "brent"} );
+	//brent_query.select("firstname").limit(1);
+
+
+	brent_query.exec(function (err, person) {
+		if (err) return handleError(err);
+	 	res.send(person.lastname);
+	});
+});
+
+app.get('/postreqs',function(req,res)
+{
+	res.sendFile('/Users/agaut/SBHacks-MABAE-/form_test.html');
+});
+
+//test processing post requests
+app.post('/postreqs',function(req,res)
+{
+	//process post request
+	//NOTE: bodyparser uses name attribute ONLY (req.body.nameAttrib) to 
+	//find particular HTML objects
+	var prof = new Profile( { firstname: req.body.firstname,
+				   lastname: req.body.lastname } );
+
+	console.log("NAME: " + prof.fullname);
+
+
+});
+
+//Start the server
+app.listen(3000, () => console.log(`Example app listening on port 3000`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
