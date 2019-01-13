0a140ca956c99af29c6e1f03fdc57f8a42c43e2c
AndrewJGaut <ajg@umail.ucsb.edu>
2019-01-13 03:59:12 -0800

    Create Individual Sites for each Project

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get("/projects/:id", function(req, res, cb){
 
 		//if no error, then we know project
 
-		res.render("single.ejs", {title: project.title, description: project.briefDescription, imageName: "/images/" + project.image}); 
+		res.render("single.ejs", {project: project}); 
 
 	});
 });
